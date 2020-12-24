d5734d0754e06ae9fb2c3b1ae8ba07f15789fc1e
Herman-Motcheyo <hermanmotcheyo@gmail.com>
2020-12-24 20:38:48 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,36 @@
+var btn= document.getElementById("done");
+btn.addEventListener("click",AddRow);
+var row=1;
+function AddRow()
+{
+ 	 var work=document.getElementById("work").value;
+  var time=document.getElementById("time").value;
+
+  var day = document.getElementById("day").value;
+
+  if(!work || !time)
+  {
+    alert("Please fill all boxes!")
+    return;
+  }
+  var mytable= document.getElementById("mytable");
+  var newRow= mytable.insertRow(row);
+  var cell1= newRow.insertCell(0);
+  var cell2= newRow.insertCell(1);
+  var cell3 = newRow.insertCell(2);
+
+  cell1.innerHTML = day;
+  cell2.innerHTML = time;
+  cell3.innerHTML =work;
+
+
+  row++;
+  work.reset();
+  day.reset();
+}
+var complete= document.getElementById("complete");
+complete.addEventListener("click",alertt);
+function alertt()
+{
+  alert("Good! Take a ScreenShot of your Time Table and follow this. Best Of Luck.")
+}
